afacfeb6ecc5478dbef90137777b4f335310af94
mateus-kent <mateusquent2003@gmail.com>
2022-02-11 00:23:23 -0300

    styles: add new images

--- src/components/CategoryList/data.ts
+++ src/components/CategoryList/data.ts
@@ -2,14 +2,14 @@ import lol from '../../assets/category_lol.jpg'
 import valorant from '../../assets/category_valorant.jpg'
 import csgo from '../../assets/category_csgo.jpg'
 import fortnite from '../../assets/category_fortnite.jpg'
-import fallguys from '../../assets/category_fallguys.jpg'
-import apex from '../../assets/category_apex.jpg'
+import justChatting from '../../assets/category_justchatting.jpg'
+import lostArk from '../../assets/category_lostark.jpg'
 
 export default [
   { name: 'League of Legends', source: lol },
   { name: 'VALORANT', source: valorant },
   { name: 'Counter-Strike: Global Offensive', source: csgo },
   { name: 'Fortnite', source: fortnite },
-  { name: 'Fall Guys', source: fallguys },
-  { name: 'Apex Legends', source: apex },
+  { name: 'Just Chatting', source: justChatting },
+  { name: 'Lost Ark', source: lostArk },
 ]
