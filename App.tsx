f7a5cbf88b80dd70b2c7f278aa499a7176823a9f
mateus-kent <mateusquent2003@gmail.com>
2022-02-10 21:41:04 -0300

    feat: configure app.tsx correctly

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native'
+import React from 'react'
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+import AppLoading from 'expo-app-loading'
+
+import { Routes } from './src/routes'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    roboto_400: Roboto_400Regular,
+    roboto_500: Roboto_500Medium,
+    roboto_700: Roboto_700Bold,
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />
+    return <AppLoading />
+  }
+  return <Routes />
+}
