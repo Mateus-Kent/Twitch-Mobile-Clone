84b288244ec1f6943cf03ff188467a75ff7261ce
mateus-kent <mateusquent2003@gmail.com>
2022-02-10 21:47:06 -0300

    feat: create the ComingSoon screen

---
+++ src/screens/ComingSoon/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Wrapper, Heading } from './styles'
+
+export function ComingSoon() {
+  return (
+    <Wrapper>
+      <Heading>Coming Soon</Heading>
+    </Wrapper>
+  )
+}
