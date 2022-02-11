cb3b2d3f7dbe19eba5fcfe68f1e53ee496c2ea6d
mateus-kent <mateusquent2003@gmail.com>
2022-02-10 21:46:40 -0300

    feat: create the CategoryList component

---
+++ src/components/CategoryList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import { Container, HeadingText } from './styles'
+
+export function CategoryList() {
+  return (
+    <Container>
+      <HeadingText></HeadingText>
+    </Container>
+  )
+}
