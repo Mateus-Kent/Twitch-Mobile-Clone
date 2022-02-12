7fd22f507afc5552af0860fbc2b73ce00bd40f1d
mateus-kent <mateusquent2003@gmail.com>
2022-02-12 16:11:53 -0300

    feat: create the StreamList component

---
+++ src/components/StreamList/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import streamThumbnail from '../../assets/stream_thumbnail.jpg'
+import StreamPreview from '../../assets/streamPreview.jpg'
+
+import {
+  List,
+  StreamContainer,
+  StreamThumbnail,
+  StreamColumn,
+  StreamRow,
+  StreamHeader,
+  StreamAvatar,
+  StreamUsername,
+  StreamDescription,
+  StreamCategory,
+  TagRow,
+  TagView,
+  TagText,
+} from './styles'
+
+export function StreamList() {
+  const StreamItem = () => (
+    <StreamContainer>
+      <StreamThumbnail source={StreamPreview} />
+
+      <StreamRow>
+        <StreamColumn>
+          <StreamHeader>
+            <StreamAvatar />
+            <StreamUsername numberOfLines={1}>mateus_quent</StreamUsername>
+          </StreamHeader>
+
+          <StreamDescription numberOfLines={1}>
+            Grindando no Lost Ark
+          </StreamDescription>
+
+          <StreamCategory numberOfLines={1}>Lost Ark</StreamCategory>
+        </StreamColumn>
+
+        <TagRow>
+          <TagView>
+            <TagText>Portuguese</TagText>
+          </TagView>
+          <TagView>
+            <TagText>RPG</TagText>
+          </TagView>
+        </TagRow>
+      </StreamRow>
+    </StreamContainer>
+  )
+
+  return (
+    <List>
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+    </List>
+  )
+}
